cb4ae5b0b36b41946437a201d024df5516c0e707
Phu Ha <therealphu@gmail.com>
2016-08-26 14:44:10 -0700

    Added description to ami

--- backup.js
+++ backup.js
@@ -2,6 +2,7 @@ var program = require('commander');
 var moment = require('moment');
 var AWS = require('aws-sdk');
 var sleep = require('sleep');
+var _ = require('lodash');
 
 program
     .version('0.1')
@@ -24,90 +25,118 @@ var ec2 = new AWS.EC2();
 for (i = 0; i < instances.length; i++) { 
     var instanceId = instances[i];
 
-    ec2.createImage({
-        InstanceId: instanceId,
-        Name: todayString + '_' + instanceId,
-        NoReboot: program.noreboot
+    ec2.describeInstances({
+        InstanceIds: [
+            instanceId,
+        ]
     }, function (err, data) {
+        var description;
+
+        if (data.Reservations && data.Reservations.length > 0) {
+            if (data.Reservations[0].Instances.length > 0) {
+                var tags = data.Reservations[0].Instances[0].Tags;
+                var nameTag =  _.find(tags, {Key: 'Name'});
+
+                description = JSON.stringify(nameTag);
+            }
+        }
+        else {
+            console.log(instanceId + ' doesn\'t exist');
+            process.exit(1);
+        }
+
         if (err) {
             console.log(err, err.stack);
             process.exit(1);
         }
         else {
-            console.log(data.ImageId.toString() + ' created from ' + instanceId);
-
-            console.log('working on deleting old ami images (' + deleteDateString + '_' + instanceId + ')')
-
-            ec2.describeImages({
-                Filters: [
-                    {
-                        Name: 'name',
-                        Values: [
-                            deleteDateString + '_' + instanceId
-                        ]
-                    }
-                ]
+            ec2.createImage({
+                InstanceId: instanceId,
+                Name: todayString + '_' + instanceId,
+                NoReboot: program.noreboot,
+                Description: description
             }, function (err, data) {
                 if (err) {
                     console.log(err, err.stack);
                     process.exit(1);
                 }
                 else {
-                    var images = data.Images;
+                    console.log(data.ImageId.toString() + ' created from ' + instanceId);
 
-                    for(im = 0; im < images.length; im++) {
-                        var imageId = images[im].ImageId;
+                    console.log('working on deleting old ami images (' + deleteDateString + '_' + instanceId + ')')
 
-                        console.log('deleting ami-' + imageId);
-
-                        ec2.deregisterImage({
-                            ImageId: imageId
-                        }, function (err, data) {
-                            if (err) {
-                                console.log(err, err.stack);
-                                process.exit(1);
+                    ec2.describeImages({
+                        Filters: [
+                            {
+                                Name: 'name',
+                                Values: [
+                                    deleteDateString + '_' + instanceId
+                                ]
                             }
-                            else {
-                                console.log(deleteDateString + '_' + instanceId + ' has been deregistered');
-                                console.log('working on deleting old snapshots (' + imageId + ')');
+                        ]
+                    }, function (err, data) {
+                        if (err) {
+                            console.log(err, err.stack);
+                            process.exit(1);
+                        }
+                        else {
+                            var images = data.Images;
 
-                                ec2.describeSnapshots({
-                                    Filters: [
-                                        {
-                                            Name: 'description',
-                                            Values: [
-                                                'Created by CreateImage(' + instanceId + ') for ' + imageId + '*'
-                                            ]
-                                        }
-                                    ]
+                            for(im = 0; im < images.length; im++) {
+                                var imageId = images[im].ImageId;
+
+                                console.log('deleting ami-' + imageId);
+
+                                ec2.deregisterImage({
+                                    ImageId: imageId
                                 }, function (err, data) {
                                     if (err) {
                                         console.log(err, err.stack);
                                         process.exit(1);
                                     }
                                     else {
-                                        var snapshots = data.Snapshots;
+                                        console.log(deleteDateString + '_' + instanceId + ' has been deregistered');
+                                        console.log('working on deleting old snapshots (' + imageId + ')');
 
-                                        for (s = 0; s < snapshots.length; s++) {
-                                            var snapshot = snapshots[s];
+                                        ec2.describeSnapshots({
+                                            Filters: [
+                                                {
+                                                    Name: 'description',
+                                                    Values: [
+                                                        'Created by CreateImage(' + instanceId + ') for ' + imageId + '*'
+                                                    ]
+                                                }
+                                            ]
+                                        }, function (err, data) {
+                                            if (err) {
+                                                console.log(err, err.stack);
+                                                process.exit(1);
+                                            }
+                                            else {
+                                                var snapshots = data.Snapshots;
 
-                                            ec2.deleteSnapshot({
-                                                SnapshotId: snapshot.SnapshotId
-                                            }, function (err, data) {
-                                                if (err) {
-                                                    console.log(err, err.stack);
-                                                    process.exit(1);
+                                                for (s = 0; s < snapshots.length; s++) {
+                                                    var snapshot = snapshots[s];
+
+                                                    ec2.deleteSnapshot({
+                                                        SnapshotId: snapshot.SnapshotId
+                                                    }, function (err, data) {
+                                                        if (err) {
+                                                            console.log(err, err.stack);
+                                                            process.exit(1);
+                                                        }
+                                                        else {
+                                                            console.log(snapshot.SnapshotId + ' has been deleted');
+                                                        }
+                                                    })
                                                 }
-                                                else {
-                                                    console.log(snapshot.SnapshotId + ' has been deleted');
-                                                }
-                                            })
-                                        }
+                                            }
+                                        });
                                     }
                                 });
                             }
-                        });
-                    }
+                        }
+                    });
                 }
             });
         }
